16ab87acb71ac84ce768505475cea16560feaf96
chrisgrasmussen <chrisrasmussenphotography@gmail.com>
2024-09-27 07:21:33 -0700

    added start table in db

--- skeleton/start/models.py
+++ skeleton/start/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Start(models.Model):
+    title = models.CharField(max_length=256)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ('-timestamp',)
+        verbose_name = ("Start")
+        verbose_name_plural = ("Starts")
